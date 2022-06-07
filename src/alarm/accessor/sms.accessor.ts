06a88f35ae7622792a0b23758312f2a243bb28c2
Ly Do <lydo1797@gmail.com>
2022-06-06 23:16:54 -0700

    Add alarm handler to send sms to phone.

---
+++ src/alarm/accessor/sms.accessor.ts
@@ -0,0 +1,44 @@
+import { SNS, STS } from "aws-sdk";
+import { PublishInput, PublishResponse } from "aws-sdk/clients/sns";
+import { AssumeRoleRequest } from "aws-sdk/clients/sts";
+
+export class SmsAccessor {
+
+    private client: SNS;
+
+    constructor() {
+    }
+
+    async init() {
+        if (this.client) {
+            return;
+        }
+
+        const sts = new STS();
+        const assumeRoleRequest: AssumeRoleRequest = {
+            RoleArn: 'arn:aws:iam::096328248765:role/ly-personal-service-send-sms-role',
+            RoleSessionName: 'send-sms',
+        }
+        const stsRes = await sts.assumeRole(assumeRoleRequest).promise();
+        const credentials = stsRes.Credentials;
+
+        this.client = new SNS({
+            region: 'us-east-1',
+            credentials: {
+                accessKeyId: credentials.AccessKeyId,
+                secretAccessKey: credentials.SecretAccessKey,
+                expireTime: credentials.Expiration,
+                sessionToken: credentials.SessionToken
+            }
+        });
+    }
+
+    sendSMS(phone: string, subject: string, message: string): Promise<PublishResponse> {
+        const params: PublishInput = {
+            Subject: subject,
+            Message: message,
+            PhoneNumber: phone
+        }
+        return this.client.publish(params).promise();
+    }
+}
---
+++ src/alarm/handler/personal-service-alarm.handler.ts
@@ -0,0 +1,24 @@
+import 'source-map-support/register';
+
+import { SNSEventRecord } from "aws-lambda";
+import { SmsAccessor } from "../accessor/sms.accessor";
+
+const smsAccessor = new SmsAccessor();
+
+export const handler = async (handlerInput: any) => {
+    console.log("PersonalServiceAlarm lambda is invoked with handlerInput", JSON.stringify(handlerInput));
+    const records: SNSEventRecord[] = handlerInput.Records;
+    const sns = records[0].Sns;
+    console.log('sns =', JSON.stringify(sns));
+
+    await smsAccessor.init();
+
+    // Send sms to my phone
+    const requests = Promise.allSettled([
+        smsAccessor.sendSMS('+17142610933', sns.Subject, sns.Message),
+        smsAccessor.sendSMS('+19493946149', sns.Subject, sns.Message),
+    ]);
+
+    const res = await requests;
+    console.log('res =', res);
+}
